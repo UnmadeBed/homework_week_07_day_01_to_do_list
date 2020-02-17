2117b296dc161d9b8b75c586af4c026ea0d5003e
unmadebed <kevin.archer@live.com>
2020-02-17 20:10:08 +0000

    render basic array in html

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({
-    el: "app"
-  })
+    el: "#app",
+    data: {
+      todos: ["Iron clothes", "Revise notes", "Food shopping"]
+    }
+  });
 });
