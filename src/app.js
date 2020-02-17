087dc5df907fce2860bad9d1b702adbaeb1fb251
unmadebed <kevin.archer@live.com>
2020-02-17 21:04:45 +0000

    arrange html layout and capture form input

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
-      todos: ["Iron clothes", "Revise notes", "Food shopping"]
+      todos: ["Iron clothes", "Revise notes", "Food shopping"],
+      newItem:""
     }
   });
 });
